8a57145718b39687e210082099f05608d08bc1cc
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-12-23 18:23:59 -0600

    Added components to manage users.

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import UsersList from '../components/UsersList'
 
 const Users = () => {
-  return (
-    <div>
-      User works
-    </div>
-  )
+
+  const USERS = [
+    {
+      id: 1,
+      name: 'oscar',
+      image: 'https://avatars1.githubusercontent.com/u/27979274?s=460&v=4',
+      places: 5
+    }
+  ]
+
+  return <UsersList items={USERS}/>
 }
 
 export default Users
